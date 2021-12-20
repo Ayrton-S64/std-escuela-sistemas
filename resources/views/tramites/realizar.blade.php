f460d21a0fc6a0dab40df567f0fc23a0e31fe8b8
Ayrton Soto <ayrtonxd.123@gmail.com>
2021-12-19 22:49:57 -0500

    Ayrton: Agregado listado de documentos seleccionados

--- resources/views/tramites/realizar.blade.php
+++ resources/views/tramites/realizar.blade.php
@@ -1,73 +1,125 @@
 @extends('layouts.plantilla')
 @section('contenido')
   <div class="row">
-      <div class="col-12">
-          <h1>Registrar un nuevo tramite</h1>
-      </div>
+    <div class="col-12">
+      <h1>Registrar un nuevo tramite</h1>
+    </div>
   </div><br>
-<form  action="{{route('tramite.store')}}" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
+  <form action="{{ route('tramite.store') }}" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
     @csrf
     {{-- No te faltó eso xd --}}
-  <div class="row ">
-    <div class="form-group col-8">
+    <div class="row ">
+      <div class="form-group col-8">
         <div class="row">
-            <h4 class="col-5 text-right">Codigo:</h4>
-            <div class="col-7">
-                <input type="text" class="form-control" id="txtCodigoAlumno" lang="es" placeholder='{{str_repeat('0',10 - strlen(auth()->id())).(auth()->id())}}' readonly>
-            </div>
+          <h4 class="col-5 text-right">Codigo:</h4>
+          <div class="col-7">
+            <input type="text" class="form-control" id="txtCodigoAlumno" lang="es"
+              placeholder='{{ str_repeat('0', 10 - strlen(auth()->id())) . auth()->id() }}' readonly>
+          </div>
         </div>
-    </div>
-    <div class="form-group col-8">
+      </div>
+      <div class="form-group col-8">
         <div class="row">
-            <h4 class="col-5 text-right">Tipo de tramite:</h4>
-            <div class="col-7">
-                <select class="form-control" id="tipoTramite" name="tipoTramite">
-                  @foreach ($tiposTramites as $tipo)
-                    <option value="{{ $tipo->id }}">{{ $tipo->descripcion }}</option>
-                  @endforeach
-                </select>
-            </div>
+          <h4 class="col-5 text-right">Tipo de tramite:</h4>
+          <div class="col-7">
+            <select class="form-control" id="tipoTramite" name="tipoTramite">
+              @foreach ($tiposTramites as $tipo)
+                <option value="{{ $tipo->id }}">{{ $tipo->descripcion }}</option>
+              @endforeach
+            </select>
+          </div>
         </div>
-    </div>
-    <div class="form-group col-8" style="height: fit-content">
+      </div>
+      <div class="form-group col-8" style="height: fit-content">
         <div class="row">
-            <h4 class="col-5 text-right">Asunto:</h4>
-            <div class="custom-file col-7" style="height: fit-content">
-                <textarea class="form-control" name="txtRazon" id="txtRazon" rows="4">
+          <h4 class="col-5 text-right">Asunto:</h4>
+          <div class="custom-file col-7" style="height: fit-content">
+            <textarea class="form-control" name="txtRazon" id="txtRazon" rows="4">
 
-                </textarea>
-            </div>
+                  </textarea>
+          </div>
         </div>
-    </div>
-    <div class="form-group col-8">
+      </div>
+      <div class="form-group col-8">
         <div class="row">
-            <h4 class="col-5 text-right">Documentos adjuntos:</h4>
-            <div class="custom-file col-7">
-                <input type="file" class="custom-file-input" name="archivosAdjuntos" id="idFileAdjunto" lang="es">
-                <label class="custom-file-label " for="customFileLang">Seleccionar Archivo</label>
-              </div>
+          <h4 class="col-5 text-right">Documentos adjuntos:</h4>
+          <div class="custom-file col-7">
+            <input type="file" class="custom-file-input" name="archivosAdjuntos" multiple id="idFileAdjunto" lang="es">
+            <label class="custom-file-label " for="customFileLang">Seleccionar Archivo</label>
+          </div>
         </div>
+        <div class="row pt-2">
+          <div id="listadoDocumentos" class="col-7 offset-5 align-self-end p-0">
+
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="row ">
-    <div class="col-4 text-center" style="">
-      <h4></h4>
+    <div class="row ">
+      <div class="col-4 text-center" style="">
+        <h4></h4>
+      </div>
+      <div class="form-group col-4">
+
+
+      </div>
+
     </div>
-    <div class="form-group col-4">
+    <div class="row justify-content-center">
+      <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
 
-
-    </div>
-
-  </div>
-  <div class="row justify-content-center">
-    <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
-
-    <a class="mx-3 btn btn-secondary" href="{{ route('vertramites') }}">
+      <a class="mx-3 btn btn-secondary" href="{{ route('vertramites') }}">
         Volver
-    </a>
-  </div>
+      </a>
+    </div>
 
-</form>
+  </form>
 
 
+@endsection
+@section('script')
+  <script>
+    var arraydocumentos = [];
+    getFiles() {
+      let fInput = document.getElementById('idFileAdjunto');
+        array_documentos = [...fInput.files].map(file => ({
+        nombre: file.name,
+        tipo: file.type,
+        size: this.formatBytes(file.size),
+        icon: (file.type === "application/pdf") ? "far fa-file-pdf" : "far fa-file-image",
+      }));
+      console.log(this.array_documentos);
+    }
+
+    listFiles(){
+        let contenedor = document.getElementById('listadoDocumentos');
+        contenedor.innerHTML = "";
+        for(let item of arraydocumentos){
+            contenedor.innerHTML += `
+            <div class="card bg-light px-2 mb-1">
+                <div class="row">
+                <div class="col-9">
+                    <p class="text-truncate w-100 m-0"><i class="${ item.icon }"></i> - ${ item.nombre }</p>
+                </div>
+                <div class="col-3 text-right">
+                    ${ item.size }
+                </div>
+                </div>
+            </div>
+            `
+        }
+    }
+
+    formatBytes(bytes, decimals = 2) {
+      if (bytes === 0) return '0 Bytes';
+
+      const k = 1024;
+      const dm = decimals < 0 ? 0 : decimals;
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+      const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
+  </script>
 @endsection
